18c44f5a6f421a2b34d5a7cf7d99c19d1976d266
Suhail Kadyrov <suhail.kadyrov@gmail.com>
2022-12-29 23:39:01 +0500

    additional information about functions

--- Cheatsheets/Microsoft SQL Server.sql
+++ Cheatsheets/Microsoft SQL Server.sql
@@ -766,6 +766,45 @@ Reason for improved performance of an inline table valued function:
 Internally, SQL Server treats an inline table valued function much like it would a view and treats a multi-statement table valued function similar to how it would a stored procedure.
 */
 
+/*
+Deterministic functions always return the same result any time they are called with a specific set of input values and given the same state of the database. 
+Examples: Sum(), AVG(), Square(), Power() and Count()
+Note: All aggregate functions are deterministic functions.
+
+Nondeterministic functions may return different results each time they are called with a specific set of input values even if the database state that they access remains the same.
+Examples: GetDate() and CURRENT_TIMESTAMP
+
+Rand() function is a Non-deterministic function, but if you provide the seed value, the function becomes deterministic, as the same value gets returned for the same seed value.
+*/
+
+
+-- Encrypting the text of the UDF
+ALTER FUNCTION {FUNC_NAME}
+...
+WITH ENCRYPTION
+AS
+...
+
+
+-- SchemaBinding
+ALTER FUNCTION fn_GetEmployeeNameById(@Id int)
+RETURNS nvarchar(20)
+WITH SCHEMABINDING
+AS
+BEGIN
+	RETURN (SELECT NAME FROM dbo.tblEmployees WHERE Id = @Id)
+END
+
+-- Schemabinding specifies that the function is bound to the database objects that it references. When SCHEMABINDING is specified, the base objects cannot be modified in any way that would affect the function definition. The function definition itself must first be modified or dropped to remove dependencies on the object that is to be modified.
+
+
+
+
+
+
+
+
+
 
 
 
